878ad5e6eaf20ef7caebaec871bf7f21300e2b43
Draghoyns <sakula.hys@polytechnique.org>
2024-05-23 17:09:17 +0900

    remove callback

--- turtlebot_test/subscriber_plain.py
+++ turtlebot_test/subscriber_plain.py
@@ -1,4 +1,5 @@
 import nep
+import time
 
 def callback(msg):
     print ("Callback get")
@@ -6,9 +7,17 @@ def callback(msg):
 
 msg_type = "string"           # Message type to listen.
 
-node = nep.node("subscriber_sample","ROS")      # Create a new node
+node = nep.node("subscriber_sample", "ROS")      # Create a new node
 
 # Listen info inside a event loop
-sub = node.new_callback("pub_sub_test", msg_type , callback)
+#sub = node.new_callback("pub_sub_test", msg_type , callback)
 
-node.spin()
+sub = node.new_sub("pub_sub_test",msg_type)
+
+while True :
+    is_message, msg = sub.listen()
+    if is_message:
+        print(msg)
+    else :
+        time.sleep(1)
+#node.spin()
