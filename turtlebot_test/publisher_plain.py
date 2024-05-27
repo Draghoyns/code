ac3b942082c8f35729d3644e860ab6c45bd71b48
Draghoyns <sakula.hys@polytechnique.org>
2024-05-27 08:11:53 +0200

    add IP

--- turtlebot_test/publisher_plain.py
+++ turtlebot_test/publisher_plain.py
@@ -5,7 +5,8 @@ import sys
 msg_type = "string"         # Message type to listen. 
 
 node = nep.node("publisher_sample","ROS")               # Create a new node
-pub = node.new_pub("pub_sub_test",msg_type)        # Set the topic and the configuration of the publisher
+conf = node.hybrid('10.105.95.233')
+pub = node.new_pub("pub_sub_test",msg_type, conf)        # Set the topic and the configuration of the publisher
 
 # Publish a message each second
 while True: 
